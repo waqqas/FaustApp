60e602561f511f1a84492092fed2baaa444b960d
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2017-08-01 12:50:29 +0500

    Removed native-base

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,30 +1,25 @@
-import React, { Component } from 'react'
-import { View, StatusBar } from 'react-native'
+import React, {Component} from 'react'
+import {StatusBar, View} from 'react-native'
 import ReduxNavigation from '../Navigation/ReduxNavigation'
-import { connect } from 'react-redux'
-import { Container } from 'native-base'
-
-
+import {connect} from 'react-redux'
 // Styles
 import styles from './Styles/RootContainerStyles'
 import StartupActions from '../Redux/StartupRedux'
 import ReduxPersist from '../Config/ReduxPersist'
 
 class RootContainer extends Component {
-  componentDidMount () {
+  componentDidMount() {
     // if redux persist is not active fire startup action
     if (!ReduxPersist.active) {
       this.props.startup()
     }
   }
 
-  render () {
+  render() {
     return (
       <View style={styles.applicationView}>
-        <StatusBar barStyle='light-content' />
-        <Container>
-        <ReduxNavigation />
-        </Container>
+        <StatusBar barStyle='light-content'/>
+        <ReduxNavigation/>
       </View>
     )
   }
